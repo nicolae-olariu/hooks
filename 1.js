02ca5b479aea3ad38421599928227c4edeab08b3
Nicolae Olariu <nicolae.olariu@xivic.com>
2016-10-21 18:52:21 +0300

    settings ready to use

--- 1.js
+++ 1.js
@@ -1,28 +1,107 @@
-function test() {
-    var total = 0,
-        len = arguments.length;
+foo(
+  "demo", {
+    title: "Demo",
+    width: 100
+  },
+  function() {
+    object
 
-    // This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line
+      .firstCall({
+        'a',
+        'b'
+      })
+      .secondCall();
+  }
+);
 
+var myLink = {
+    img: "btn.gif",
+    text: "Button",
+    width: 128
+  },
+  local = true,
+  initial = -1;
+var selector = "#id";
+var color = "red";
+var offset = 10;
 
+varName = val;
 
+import { Component } from 'react'
 
+function foo(x, y, z) {
+  bar(1, b);
 
+  var i = 0,
 
+    x = { 0: "zero", 1: "one" },
+    a = [0, 1, 2],
+    foo = function() {};
 
-
-
-    for (i = 0; i < len; ++i) {
-        total += arguments[i];
+  if (!i > 10) {
+    for (var j = 0; j < 10; j++) {
+      switch (j) {
+        case 0:
+          value = "zero";
+          break;
+        case 1:
+          value = "one";
+          break;
+      }
+      var c = j > 5 ? "GT 5" : "LE 5";
     }
+  } else {
+    var j = 0;
+    try {
+      while (j < 10) {
+        if (i == j || j > 5) {
+          a[j] = i + j * 12;
+        }
 
+        i = (j << 2) & 4;
+        j++;
+      }
+      do {
+        j--;
+      } while (j > 0)
+    } catch (e) {
+      alert("Failure: " + e.message);
+    } finally {
+      reset(a, i);
+    }
+  }
+}
 
+function test(a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a,
+  a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a,
+  a, a, a, a) {
+  var total = 0,
+    len = arguments.length,
+    obj = { prop: 1, test: 2, ceva: 3, bla: { incaceva: 23 } },
+    test = function() {
+      return {
+        incaceva: function() {
+          return this;
+        },
+        bla: function() {
+          return this;
+        }
+      };
+    };
 
+  function test() {
+    console.log('this is just a test!');
+  }
 
+  // This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line This is a very long line
 
+  for (i = 0; i < len; ++i) {
+    total += arguments[i];
+  }
 
+  console.log(test()
+    .incaceva()
+    .bla());
 
-
-
-    return total;
+  return total;
 }
---
+++ 2.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function() {
+  var a = b = 3;
+})();
